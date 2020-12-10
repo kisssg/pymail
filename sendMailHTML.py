cdb6c425f96e17c295d475fa85c3f0f7c5c811b3
Sucre XU (CN) <Sucre.XU@homecredit.cn>
2020-12-10 10:43:15 +0800

    template update

--- sendMailHTML.py
+++ sendMailHTML.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#! update at 2020-12-10 9:59 by sucre
 
 import datetime
 import smtplib
@@ -20,19 +20,21 @@ me = os.getenv("from", "yourname@yourserver.com")
 #you = ["xxx@homecreditcfc.cn"]
 book = xlrd.open_workbook('template.xlsx')
 sheet = book.sheet_by_name('Sheet1')
-data = [[sheet.cell_value(r, c) for c in range(sheet.ncols)] for r in range(sheet.nrows)]
-data=pd.DataFrame(data,None,data[0])
+data = [[sheet.cell_value(r, c) for c in range(sheet.ncols)]
+        for r in range(sheet.nrows)]
+data = pd.DataFrame(data, None, data[0])
 book.__exit__
-result={}
-for i in range(1,len(data)):
-    #print(['name','email','msg1','msg2'])
+result = {}
+for i in range(1, len(data)):
+    # print(['name','email','msg1','msg2'])
     you = data['接收人邮箱'][i].split(',')
 
     # Create message container - the correct MIME type is multipart/alternative.
     COMMASPACE = ', '
     msg = MIMEMultipart('alternative')
     # 这里是输入邮件标题
-    msg['Subject'] = "音视频核查结果通知{}-{}".format(data['合同号码'][i],xlrd.xldate.xldate_as_datetime(data['外访日期'][i],0).date())
+    msg['Subject'] = "音视频核查结果通知{}-{}".format(
+        data['合同号码'][i], xlrd.xldate.xldate_as_datetime(data['外访日期'][i], 0).date())
     msg['From'] = me
     msg['To'] = COMMASPACE.join(you)
     # msg['Cc'] = me
@@ -42,19 +44,21 @@ for i in range(1,len(data)):
     # Create a text/plain message
     # text_HTML = fp.read().decode('utf-8')
     # fp.close()
-    template="""
+    template = """
     <body>
     Dear {0},<br/>
     <br/>
+    通过对您外访音视频的抽查，我们发现了一些需您改进的行为，详情请见下表：
     <table cellpadding="5" cellspacing="0" border="1"> 
     <tr><td>员工工号</td><td>{1}</td></tr>
     <tr><td>质检工号</td><td>{2}</td></tr>
+    <tr><td>核查日期</td><td>{8}</td></tr>
     <tr><td>合同号码</td><td>{3}</td></tr>
     <tr><td>外访日期</td><td>{4}</td></tr>
     <tr><td>外访时间</td><td>{5}</td></tr>
     <tr><td>无效外访申诉结果</td><td>{6}</td></tr>
     <tr><td>此音视频中的错误</td><td>{7}</td></tr>
-    <tr><td>说明</td><td>核查出的违规将提出扣罚建议。</td></tr>
+    <tr><td>说明</td><td>{9}</td></tr>
     <tr><td>正确指引提示</td><td>请参考《错误行为正确做法指引V2.0》</td></tr>
     <tr><td>如果您对此错误有疑问</td><td>如果您对此错误有疑问，您可以联系您的组长协助查看视频。</td></tr>
     <tr><td>如果您需要申诉</td><td>如果您需要申诉，您需要记录视频中关键行为的时间点，按照申诉邮件模板回复进行申诉。 </td></tr>
@@ -63,7 +67,9 @@ for i in range(1,len(data)):
     后期催收质检团队
     </body>
     """
-    text_HTML=template.format(data['员工姓名'][i],data['员工工号'][i],data['QC工号'][i],data['合同号码'][i],xlrd.xldate.xldate_as_datetime(data['外访日期'][i],0).date(),data['外访时间'][i],data['无效外访申诉结果'][i],data['此音视频中的错误'][i])
+    text_HTML = template.format(data['员工姓名'][i], data['员工工号'][i], data['QC工号'][i], data['合同号码'][i], xlrd.xldate.xldate_as_datetime(
+        data['外访日期'][i], 0).date(), data['外访时间'][i], data['无效外访申诉结果'][i], data['此音视频中的错误'][i], 
+        xlrd.xldate.xldate_as_datetime(data['核查日期'][i],0).date(), data['说明'][i])
 
     # Create the body of the message (a plain-text and an HTML version).
     # text = "Daily report"
@@ -84,9 +90,9 @@ for i in range(1,len(data)):
     s = smtplib.SMTP(host=host, port=port)
     # sendmail function takes 3 arguments: sender's address, recipient's address
     # and message to send - here it is sent as one string.
-    sendresult=s.sendmail(me, you, msg.as_string())
-    result = {**result, **sendresult}  
-if(result=={}):
+    sendresult = s.sendmail(me, you, msg.as_string())
+    result = {**result, **sendresult}
+if(result == {}):
     print("All done successfully!")
 else:
     for r in result:
