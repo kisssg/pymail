e5f39103e1e4b0c813e4d3889a2b474c6bad7097
kisssg <shuige@foxmail.com>
2023-02-27 14:03:43 +0800

    Update readExcel.py

--- readExcel.py
+++ readExcel.py
@@ -2,7 +2,7 @@ import xlwings as xw
 import pandas as pd
 # app = xw.App(visible= True, add_book= False)
 worksheet = xw.Book('template.xlsx').sheets[0]
-column = ["员工工号","接收人邮箱"]
+column = ["employee_id","mail_to"]
 values = worksheet.range('A1').expand().options(pd.DataFrame,index=False).value
 filtered=values[column]
 print(filtered)
