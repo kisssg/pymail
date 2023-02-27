0f341b9af30bcc0356ff55eb2fe04033aff89317
kisssg <shuige@foxmail.com>
2023-02-27 14:04:50 +0800

    Delete readExcel.py

--- readExcel.py
+++
@@ -1,9 +0,0 @@
-import xlwings as xw
-import pandas as pd
-# app = xw.App(visible= True, add_book= False)
-worksheet = xw.Book('template.xlsx').sheets[0]
-column = ["employee_id","mail_to"]
-values = worksheet.range('A1').expand().options(pd.DataFrame,index=False).value
-filtered=values[column]
-print(filtered)
-# workbook.close()
