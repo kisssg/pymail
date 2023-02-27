930b0e86c8cf8acc3530c1c0607a0e357a457e44
kisssg <shuige@foxmail.com>
2023-02-27 14:04:41 +0800

    Delete read_excel.py

--- read_excel.py
+++
@@ -1,7 +0,0 @@
-import xlrd
-import pandas as pd
-book = xlrd.open_workbook('template.xlsx')
-sheet = book.sheet_by_name('Sheet1')
-data = [[sheet.cell_value(r, c) for c in range(sheet.ncols)] for r in range(sheet.nrows)]
-data=pd.DataFrame(data,None,data[0])
-print(xlrd.xldate.xldate_as_datetime((data['外访日期'][1]), 0).time())
